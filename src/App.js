a268434407f197b1aaa52391c37004cb65b4abff
imhjnoh <imhjnoh@gmail.com>
2022-07-23 09:16:04 +0900

    Added: header

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
-import { ThemeProvider } from '@mui/material';
-import {theme} from "./Styles.js"
-import {Routes, Route} from "react-router-dom"
+import { ThemeProvider, Button, ButtonGroup, Toolbar, Box } from '@mui/material';
+import {theme, Logo, Header} from "./Styles.js"
+import {Routes, Route, useNavigate, useLocation} from "react-router-dom"
 
 function App() {
+  const navigate = useNavigate()
+  const location = useLocation()
   return (
     <div className="App">
       <ThemeProvider theme = {theme}>
+      {!(
+          location.pathname.startsWith("/sign_in") ||
+          location.pathname.startsWith("/sign_up")
+        ) && (<Header position="static">
+            <Toolbar>
+              <Logo/>
+
+              <Box sx={{ flexGrow: 1 }} />
+              {/* {isLogin && <Avatar src={ user?.profileSrc ?  user?.profileSrc: MoominIcon}></Avatar>}
+              {isLogin && <Typography sx={{mx: 2}}>{user?.name}님, 환영합니다!</Typography>} */}
+
+
+              {true && (
+                <ButtonGroup
+                variant="text" 
+                  aria-label="outlined primary button group"
+                >
+                  {/* {!isLogin && ( */}
+                    <Button onClick={() => navigate("/sign_up")}>
+                      회원가입
+                    </Button>
+                  {/* )} */}
+                  {/* {!isLogin && ( */}
+                    <Button onClick={() => navigate("/sign_in")}>로그인</Button>
+                  {/* )} */}
+                  {/* {isLogin && <Button onClick={logout}>로그아웃</Button>}
+                  {isLogin && <Button onClick={addpost}>글쓰기</Button>} */}
+                </ButtonGroup>
+              )}
+            </Toolbar>
+          </Header>)}
         <Routes>
-          <Route path="/*" element={<SignIn/>} />
-          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/sign_in" element={<SignIn/>} />
+          <Route path="/sign_up" element={<SignUp/>} />
         </Routes>
       </ThemeProvider>
     </div>
