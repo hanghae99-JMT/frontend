96170f252ce83acf41467948907a2f5959af5c95
102wy <tser5@naver.com>
2022-07-23 09:20:16 +0900

    마이 페이지 퍼블리싱

---
+++ src/component/mypage/mypage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './mypage.module.scss';
+import { Container } from '@material-ui/core';
+import Store from './store';
+
+const MyPage = () => {
+    return (
+        <Container className={styles.mypage}>
+            <section className={styles.user}>
+                <p>User Name</p>
+                <p>1234@gmail.com</p>
+            </section>
+            <section className={styles.tab}>
+                {/* <ul className={styles.tab_menu}>
+                    <li>좋아요 한 가게</li>
+                    <li>후기</li>
+                </ul> */}
+                <ul className={styles.store}>
+                    <Store />
+                </ul>
+            </section>
+        </Container>
+    );
+};
+
+export default MyPage;
